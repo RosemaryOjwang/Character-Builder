6e312bc96484b97a308a4571d498112497d1850e
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-01-04 10:21:43 +0300

    add conditions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 //User Interface
 function displayAvenger(event)   {
     event.preventDefault();
-    const avenger = document.querySelector("option#");
+    const avenger = document.querySelector("#character").value;
     if (avenger === 'Superman') {
-    
+        <img src="IMG/Super_man.png" alt="superman"></img>
+    } else if (avenger === 'Hulk')  {
+        <img src="IMG/Hulk.png" alt="Hulk"></img>
+    } else if (avenger === 'Ironman')   {
+        <img src="IMG/Iron_man.png" alt="Iron man"></img>
+    } else if (avenger === 'Captainamerica')    {
+        <img src="IMG/Captain_america.png" alt="Captain America"></img>
+    } else if (avenger === 'Spiderman') {
+        <img src="IMG/Spider_man.png" alt="Spiderman"></img>
+    } else if (avenger === 'Batman')    {
+        <img src="IMG/Bat_man.png" alt="Batman"></img>
     }
 }
 
