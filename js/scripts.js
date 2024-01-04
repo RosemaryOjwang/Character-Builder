083fe536d4e3c74b371e5d4bdd893ca8a61d50dc
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-01-04 16:22:19 +0300

    edit function to display avenger images if selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 //User Interface
 function displayAvenger(event)   {
     event.preventDefault();
-    const avenger = document.querySelector("#character").value;
-    if (avenger === 'Superman') {
-        <img src="IMG/Super_man.png" alt="superman"></img>
-    } else if (avenger === 'Hulk')  {
-        <img src="IMG/Hulk.png" alt="Hulk"></img>
-    } else if (avenger === 'Ironman')   {
-        <img src="IMG/Iron_man.png" alt="Iron man"></img>
-    } else if (avenger === 'Captainamerica')    {
-        <img src="IMG/Captain_america.png" alt="Captain America"></img>
-    } else if (avenger === 'Spiderman') {
-        <img src="IMG/Spider_man.png" alt="Spiderman"></img>
-    } else if (avenger === 'Batman')    {
-        <img src="IMG/Bat_man.png" alt="Batman"></img>
+    
+    let selection = document.getElementById("character").value;
+    document.getElementById("avengerName").innerHTML = selection;
+    
+    if (selection === 'Superman') {
+        document.getElementById("avenger1").style.display;
+    } else if (selection === 'Hulk')  {
+        document.getElementById("avenger4").style.display;
+    } else if (selection === 'Ironman')   {
+        document.getElementById("avenger3").style.display;
+    } else if (selection === 'Captainamerica')    {
+        document.getElementById("avenger5").style.display;
+    } else if (selection === 'Spiderman') {
+        document.getElementById("avenger2").style.display;
+    } else if (selection === 'Batman')    {
+        document.getElementById("avenger6").style.display;
     }
 }
+window.addEventListener("load", function()  {
+    console.log(selection);
+    const form = document.getElementById("characterChoices");
+    form.addEventListener("submit", displayAvenger);
+});
+
 
 
  
