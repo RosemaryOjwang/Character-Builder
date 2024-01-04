69e73ba447a63ce18f2526fb978f329380057247
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-01-05 01:02:05 +0300

    edit code

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,39 @@
 //Business logic
-let mySelection = document.getElementById("character").value;
+
 function displaySuperman()  {
     
     document.getElementById("avenger1").style.display;
 }
 
 function displaySpiderman() {
-    document.getElementById("avenger2").style.display;
+    document.getElementById("avenger2").style.display = "block";
 }
 
 function displayIronman()   {
-    document.getElementById("avenger3").style.display;
+    document.getElementById("avenger3").style.display = "block";
 }
 
 function displayHulk()  {
-    document.getElementById("avenger4").style.display;
+    document.getElementById("avenger4").style.display = "block";
 }
 
 function displayCaptainAmerica()    {
-    document.getElementById("avenger5").style.display;
+    document.getElementById("avenger5").style.display = "block";
 }
 
 function displayBatman()    {
-    document.getElementById("avenger6").style.display;
+    document.getElementById("avenger6").style.display = "block";
 }
 
 
 //User Interface
-function displayAvenger(event)   {
+/*function displayAvenger(event)   {
+    
     event.preventDefault();
+    let mySelection = document.getElementById("character").value;
+    console.log(mySelection);
     
-    
-    document.getElementById("avengerName").innerHTML = selection;
+    document.getElementById("avengerName").innerHTML = mySelection;
     
     if (selection === 'Superman') {
         displaySuperman();
@@ -46,13 +48,22 @@ function displayAvenger(event)   {
     } else if (selection === 'Batman')    {
         displayBatman();
     }
+}*/
+/*window.addEventListener("load", (event) => {
+    event.preventDefault();
+    console.log("page fully loaded");
+}  /*{
+    const dropdown = document.getElementById("character");
+    displayAvenger();
+    dropdown.addEventListener("change", function()  {
+        alert(this.mySelection);
+    }, false);
+    console.log(mySelection);*/
+//}
+//);
+window.onload = (event) => {
+    console.log("Page is fully loaded");
 }
-window.addEventListener("load", function()  {
-    console.log(selection);
-    const form = document.getElementById("characterChoices");
-    form.addEventListener("submit", displayAvenger);
-});
-
 
 
  
